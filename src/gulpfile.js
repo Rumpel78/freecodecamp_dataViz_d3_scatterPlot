0488386dc8a8d6a58d818f08b8213ce4587c064c
Thomas Rzipa <t.rzipa@adrom.at>
2018-06-19 15:43:56 +0200

    remove basepath, make all paths relative

--- src/gulpfile.js
+++ src/gulpfile.js
@@ -14,7 +14,6 @@ var rimraf = require('gulp-rimraf');
 var concat = require('gulp-concat');
 var replace = require('gulp-replace');
 var reload = browserSync.reload;
-require('dotenv').load();
 
 // Browser
 gulp.task('browser-sync', function() {
@@ -79,7 +78,6 @@ gulp.task('compress:css', function() {
 var envReplace = function (env) {
   return gulp.src(['index.html'])
     .pipe(replace('__REPLACE_ENV__', env))
-    .pipe(replace('__BASEPATH__', process.env.BASEPATH || ''))
     .pipe(gulp.dest('./build'));
 }
 
